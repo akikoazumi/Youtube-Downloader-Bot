925ee2922c8709454de05059ce0fbd8bfcdc5ce5
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-06-21 15:17:26 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,12 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/akarida")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "panduan bot", url="https://telegra.ph/Panduan-bot-YouTube-06-21")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hai <b>{message.from_user.first_name}</b>\n/saya adalah bot pengunduh video Youtube Cukup kirim tautan video")
+
+
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
