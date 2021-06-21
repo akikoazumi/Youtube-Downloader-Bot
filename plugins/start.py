80d20602bd42f976dc7285fc668c45730fee7a6f
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-06-21 15:40:30 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,8 +9,7 @@ async def start(client, message):
         [InlineKeyboardButton(
             "panduan bot", url="https://telegra.ph/Panduan-bot-YouTube-06-21")]
     ])
-    welcomed = f"Hai <b>{message.from_user.first_name}</b>\n/saya adalah bot pengunduh video Youtube Cukup kirim tautan video")
-
+    welcomed = f"Hai <b>{message.from_user.first_name}</b>\n/saya adalah bot pengunduh video Youtube Cukup kirim tautan video"
 
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
