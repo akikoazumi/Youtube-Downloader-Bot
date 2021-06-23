2fed1bb6b79d17be6a4e600fe9e5c5e0557c23e9
akikoazumi <86140455+akikoazumi@users.noreply.github.com>
2021-06-23 11:41:22 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,9 +6,9 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/akarida")],
-        [InlineKeyboardButton(
-            "panduan bot", url="https://telegra.ph/Panduan-bot-YouTube-06-21")]
-    ])
+     ])
+
+
     welcomed = f"Hai <b>{message.from_user.first_name}</b>\nsaya adalah bot pengunduh video Youtube Cukup kirim tautan video"
 
     await message.reply_text(welcomed, reply_markup=joinButton)
